75e00829449889903e04eb8bd73359d5c98b3ea5
Tay Kong Yu <kongyutay@gmail.com>
2024-07-08 22:47:40 +0800

    let the window become more smooth on any os

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const {app, BrowserWindow} = require('electron')
 //BrowserWindow 是构造函数
 
-app.on('ready', () => {
+function createWindow() {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
@@ -10,4 +10,22 @@ app.on('ready', () => {
         //alwaysOnTop: true
     })
     win.loadFile('./pages/index.html')
+}
+
+app.on('ready', () => {
+    //如果应用准备好了，会直接创建窗口
+    createWindow();
+    
+    //让它更像mac窗口（还会出现在dock）
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+        //当窗口被激活的时候，判断现在有没有窗口，创建窗口，所以要封装createWindow函数
+    })
 })
+
+//让它更像window窗口（全关）
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+        //!== darwin 如果不是苹果
+})
+
